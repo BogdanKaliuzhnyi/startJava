6c6a7e1506f5877f65d476c5b0889ebe27a11e65
BogdanKaliuzhnyi <kaliuzhnb@gmail.com>
2024-10-16 09:03:04 +0300

    Lesson 1. Task 10 homework. MyFirstGame (v1)

---
+++ src/lesson_1/MyFirstGame.java
@@ -0,0 +1,22 @@
+package lesson_1;
+
+import java.util.Scanner;
+
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int magicNumber = (int) (Math.random() * 100) + 1;
+        System.out.println("Я загадал число от 0 до 100, попробуй его угадать");
+        System.out.print("Введи число: ");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int number = sc.nextInt();
+            if (number == magicNumber) {
+                System.out.println("Вы победили!");
+                break;
+            }
+            System.out.println(number + (number > magicNumber ? " больше " : " меньше ") +
+                    "того, что загадал компьютер");
+            System.out.print("Попробуй ещё: ");
+        }
+    }
+}
