38f7a130b581b62f75a25ffe0f17cf190fea729b
BogdanKaliuzhnyi <kaliuzhnb@gmail.com>
2024-10-22 07:41:39 +0300

    Lesson 2. Task 2 homework. Person.class

---
+++ src/lesson_2/Person.java
@@ -0,0 +1,31 @@
+package lesson_2;
+
+public class Person {
+    String sex = "Мужской";
+    String name = "Бобби";
+    float height = 1.84f;
+    float weight = 88.5f;
+    int age = 27;
+
+    void walk() {
+        System.out.println(name + " решил немного пройтись по окрестностям.");
+    }
+
+    void sit() {
+        System.out.println(name + " немного устал и присел отдохнуть.");
+    }
+
+    void run() {
+        System.out.println(name + " внезапно побежал.");
+    }
+
+    void speak() {
+        System.out.println(name + " произносит воодушевляющую речь.");
+    }
+
+    void learnJava() {
+        System.out.println(name + " продолжил изучать Java.");
+    }
+}
+
+
