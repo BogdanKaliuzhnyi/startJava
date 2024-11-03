86b3ae2144ea413fc3479cd64d08281aa44279cb
BogdanKaliuzhnyi <kaliuzhnb@gmail.com>
2024-11-03 11:17:35 +0300

    Lesson 2. Final homework 2. Jaeger modification (v.1)

---
+++ src/lesson_2/JaegerTest.java
@@ -0,0 +1,25 @@
+package lesson_2;
+
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger j1 = new Jaeger();
+        Jaeger j2 = new Jaeger("Crimson Typhoon" , "Mark-4", "China", 76.2f,
+                1.72f, 9, 8, 6);
+
+        j1.setModelName("Gipsy Danger");
+        j1.setMark("Mark-3");
+        j1.setOrigin("USA");
+        j1.setHeight(79.25f);
+        j1.setWeight(1.98f);
+        j1.setSpeed(7);
+        j1.setStrength(8);
+        j1.setArmor(6);
+
+        j1.move(j1.getSpeed());
+        j2.move(j2.getSpeed());
+        j1.isActiveDrift();
+        j2.isActiveDrift();
+        System.out.println(j1.scanKaiju());
+        j2.useCannon();
+    }
+}
