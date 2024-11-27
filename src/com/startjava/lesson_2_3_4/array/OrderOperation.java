50c29c937b943e46907570f00d73566d494630c1
BogdanKaliuzhnyi <kaliuzhnb@gmail.com>
2024-11-27 09:07:26 +0300

    Lesson 4 HW: ex1 and ex2  (v2)

--- src/com/startjava/lesson_2_3_4/array/OrderOperation.java
+++ src/com/startjava/lesson_2_3_4/array/OrderOperation.java
@@ -2,7 +2,7 @@ package com.startjava.lesson_2_3_4.array;
 
 import java.util.Arrays;
 
-public class orderOperation {
+public class OrderOperation {
     public static void main(String[] args) {
         int[] emptyArray = new int[0];
         makeReversed(emptyArray);
